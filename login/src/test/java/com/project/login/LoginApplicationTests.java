3496eb9fe05429f1bc7c012a59857a426f153800
DESKTOP-KK5SP41\新年前夜 <mascheny@naver.com>
2023-03-04 18:45:49 +0900

    Test code for join validatation logic

--- login/src/test/java/com/project/login/LoginApplicationTests.java
+++ login/src/test/java/com/project/login/LoginApplicationTests.java
@@ -7,13 +7,82 @@ import org.springframework.boot.test.context.SpringBootTest;
 class LoginApplicationTests {
 
 	@Test
-	void contextLoads() {
-		//given
-		//when
-		//then
+	public void memberIdValidate() {
+		//Given
+		Member member1 = new Member();
+		member1.setMemberId("testId").setPassword("a123456").setEmail("test@test.com");
 
-		//comment by mr-silvester
+		Member member2 = new Member();
+		member2.setMemberId("testId");
 
+		memberService.join(member1);
+
+		//When
+		IllegalStateException e = memberService.memberIdValidate(member2);
+
+		//Then
+		Assert.assertThat(e.getMessage(), is("유효하지 않은 아이디입니다."));
+		System.out.println(
+				"중복여부 : " + memberIdDupCheck + ", " + 		//중복 아이디 사용불가
+				"문자제한 : " + memberIdTypCheck + ", " + 		//한글, 특수문자 사용불가
+				"길이제한 : " + memberIdLenCheck					//최소2글자, 최대 12글자
+		);
 	}
 
+	@Test
+	public void passwordValidate() {
+		//Given
+		Member member = new Member();
+		member.setPassword("ab");
+
+		//When
+		IllegalStateException e = memberService.passwordValidate(member);
+
+		//Then
+		Assert.assertThat(e.getMessage(), is("유효하지 않은 비밀번호입니다."));
+		System.out.println(
+				"길이제한 : " + passwordLenCheck					//최소 8글자, 최대 12글자
+		);
+	}
+
+	@Test
+	public void passwordCheck() {
+		//Given
+		Member member = new Member();
+		member.setPassword("a12345689").setPasswordCheck("123456");
+
+		//When
+		IllegalStateException e = memberService.passwordCheck(member);
+
+		//Then
+		Assert.assertThat(e.getMessage(), is("비밀번호가 서로 다릅니다."));
+	}
+
+	@Test
+	public void emailValidate() {
+		//Given
+		Member member = new Member();
+		member.setEmail("test@test.com");
+
+		//When
+		IllegalStateException e = memberService.emailValidate(member);
+
+		//Then
+		Assert.asserThat(e.getMessage(), is("유효하지 않은 이메일입니다."));
+	}
+
+	@Test
+	public void emptyCheck() {
+		//Given
+		Member member = new Member();
+		member.setMemberId("test").setPassword("a123456789");
+
+		//When
+		IllegalStateException e = memberService.emptyCheck(member);
+
+		//Then
+		Assert.asserThat(e.getMessage(), is("요구된 모든 정보를 입력해주세요."));
+	}
+
+	//모든 예외를 illegalStateException으로 통일시켰기 때문에 추후 서로 다른 예외 메시지를 위해 수정이 필요합니다.
 }
