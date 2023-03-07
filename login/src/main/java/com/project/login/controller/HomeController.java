69ed57298a335809ece067efd892832bdcb9aa6d
DESKTOP-L7LPVOB\heinj <jennycookies5353@gmail.com>
2023-03-06 23:45:34 -0500

    HomeController 코멘트 오타 수정
    home => welcome

--- login/src/main/java/com/project/login/controller/HomeController.java
+++ login/src/main/java/com/project/login/controller/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
     @GetMapping("/")
     public String home() {
-        return "welcome"; //home.html 호출
+        return "welcome"; //welcome.html 호출
     }
 }
