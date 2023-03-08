d5b2f42128f6343535c548df0c6f6e24336c34df
DESKTOP-L7LPVOB\heinj <jennycookies5353@gmail.com>
2023-03-08 00:17:03 -0500

    Login Test Cases Added

--- login/src/test/java/com/project/login/LoginApplicationTests.java
+++ login/src/test/java/com/project/login/LoginApplicationTests.java
@@ -9,6 +9,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.hamcrest.CoreMatchers.is;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 @SpringBootTest
@@ -87,4 +89,18 @@ class LoginApplicationTests {
 	}
 
 	//모든 예외를 illegalStateException으로 통일시켰기 때문에 추후 서로 다른 예외 메시지를 위해 수정이 필요합니다.
+
+	//------------------------------------------------------------------------------------------------------------//
+
+	/*
+	1. ID 중복 확인
+	2. 비번 & 비번확인 일치 => html
+	3. 이메일 중복 확인 => html
+	4. 빈칸 확인
+	 */
+
+	public void validateID() {
+		Member member
+	}
+
 }
