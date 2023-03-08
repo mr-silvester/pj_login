888b46c013425cdf9872c36b4fde13ccad46c4d9
DESKTOP-KK5SP41\新年前夜 <mascheny@naver.com>
2023-03-08 13:45:32 +0900

    memory repository 생성

--- login/src/main/java/com/project/login/repository/MemoryRepository.java
+++ login/src/main/java/com/project/login/repository/MemoryRepository.java
@@ -1,5 +1,40 @@
 package com.project.login.repository;
 
+import com.project.login.entity.Member;
+
+import java.util.*;
+
 public class MemoryRepository implements MemberRepository {
+    private static Map<Long, Member> store = new HashMap<>();
+    private static Long sequence = 000L;
+
+    private Long id; //PK
+    private String memberId;
+    private String password;
+    private String email;
+
+    //@Override
+    public Member save(Member member) {
+        member.setId(sequence++);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    //@Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    //@Override
+    public Optional<Member> findByMemberId(String MemberId) {
+        return Optional.ofNullable(store.values().stream().filter(<String memberId>));
+    }
+
+    //@Override
+    public List<Member> findAll(String MemberId) {
+
+        return new ArrayList<>(store.values());
+    }
+
 
 }
