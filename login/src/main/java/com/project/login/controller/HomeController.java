d1f11b4325abc8c6fcb52f4d116f8040fd8f90bc
DESKTOP-L7LPVOB\heinj <jennycookies5353@gmail.com>
2023-03-08 21:57:17 -0500

    Edited html files and mapped them to the MemberController

--- login/src/main/java/com/project/login/controller/HomeController.java
+++ login/src/main/java/com/project/login/controller/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
     @GetMapping("/")
     public String home() {
-        return "welcome"; //welcome.html 호출
+        return "home"; //home.html 호출
     }
 }
--- login/src/main/java/com/project/login/controller/MemberController.java
+++ login/src/main/java/com/project/login/controller/MemberController.java
@@ -1,5 +1,26 @@
 package com.project.login.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class MemberController {
     //member 정보 (id & pw) 전달 받아서 Member object에 저장하고 call memberService.join(member) method
+
+//    @PostMapping("login")
+//    public String create(MemberForm form) {
+//
+//        return "welcome";
+//    }
+
+    @GetMapping("new")
+    public String createForm() {
+        return "form";
+    }
+
+//    @PostMapping("new")
+//    public String create(MemberForm form) {
+//
+//    }
 }
